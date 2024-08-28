5a43272bc9d418347f9cebeb26b12b8743b8460b
Sudip Kumar <keya24gadu2020@gmail.com>
2024-08-29 02:21:00 +0600

    variable rules

---
+++ JAVASCRIPT/variable.js
@@ -0,0 +1,17 @@
+// Rules to declare Variables
+
+// No keyword in variable name
+var false=40;
+
+// No space or gape in variable name
+var is happy=true;
+
+// No quote
+var "address"="abs";
+
+// Can't start with number but other than the first letter is allowed
+var 1money=100000;
+
+// Name is case secsitive
+var abc;
+var aBC; Both are different;
