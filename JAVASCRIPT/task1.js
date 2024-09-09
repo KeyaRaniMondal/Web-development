0b2dcc7f00903e521fc575d49bfc0e5a57a0a6c1
Sudip Kumar <keya24gadu2020@gmail.com>
2024-09-09 14:39:44 +0600

    dublicate elements

--- JAVASCRIPT/task1.js
+++ JAVASCRIPT/task1.js
@@ -1,31 +1,28 @@
 // convert inch to feet
-function inchToFeet(inch)
-{
-    const feet=inch/12
+function inchToFeet(inch) {
+    const feet = inch / 12
     return feet
 }
-const height=inchToFeet(75)
+const height = inchToFeet(75)
 console.log(height)
 
-function inchToFeet2(inch)
-{
+function inchToFeet2(inch) {
     //converts decimal nuumber into integer number. 6.25 would be shown as 6
-    const feet2=inch/12
-    const feetnum=parseInt(feet2)
-    const inchi=inch%12
-    const result=feetnum+' ft '+inchi+' inch '
+    const feet2 = inch / 12
+    const feetnum = parseInt(feet2)
+    const inchi = inch % 12
+    const result = feetnum + ' ft ' + inchi + ' inch '
     return result
 }
-const height2=inchToFeet2(75)
+const height2 = inchToFeet2(75)
 console.log(height2)
 
 
 
 
 // Mile to kilometer conversion
-function mileToKilometer(mile)
-{
-    const kilo=mile*1.60934
+function mileToKilometer(mile) {
+    const kilo = mile * 1.60934
     return kilo
 }
 console.log(mileToKilometer(1000))
@@ -34,9 +31,8 @@ console.log(mileToKilometer(1000))
 
 // fulty technique
 // Year will be leap year if it is divisible by 4
-function isLeapYear(year)
-{
-    if(year %4==0)
+function isLeapYear(year) {
+    if (year % 4 == 0)
         return true
     else
         return false
@@ -44,16 +40,31 @@ function isLeapYear(year)
 console.log(isLeapYear(2043))
 
 // justified technique
-function isLeapYear(year)
-{
+function isLeapYear(year) {
     // if((year %100!=0 && year%4==0) || year%400==0)
     //     return true
 
-    if(year %100!=0 && year%4==0)
+    if (year % 100 != 0 && year % 4 == 0)
         return true
-    else if(year%100==0 && year%400==0)
+    else if (year % 100 == 0 && year % 400 == 0)
         return true
     return false
 }
 console.log(isLeapYear(2024))
 
+
+
+
+
+// Remove Dublicate elements from an array
+
+function unique(items) {
+    const unq = []
+    for (const item of items) {
+        if (unq.includes(item) === false)
+            unq.push(item)
+    }
+    return unq
+}
+const dub_array = [1, 3, 5, 2, 5, 7, 9, 1]
+console.log(unique(dub_array))
