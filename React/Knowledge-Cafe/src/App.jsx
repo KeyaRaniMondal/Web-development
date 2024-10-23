afd32c3bd75bcc5e55a394281c825be642841f29
Sudip Kumar <keya24gadu2020@gmail.com>
2024-10-24 01:53:10 +0600

    added prop types

--- React/Knowledge-Cafe/src/App.jsx
+++ React/Knowledge-Cafe/src/App.jsx
@@ -1,13 +1,18 @@
 import Header from './components/header/header'
 import Blogs from './components/Blogs/Blogs'
 import './App.css'
+import Bookmark from './components/Bookmarks/Bookmark'
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <div className='flex'>
       <Blogs></Blogs>
+      <Bookmark></Bookmark>
+      </div>
+
     </>
   )
 }
